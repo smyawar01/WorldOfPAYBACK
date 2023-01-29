4cc50510988cf62c9ac8ac91682c2815aa216aed
Yawar M <muhammad.yawar@10pearls.com>
2023-01-29 20:12:52 +0500

    - Add first transaction list screen

---
+++ WorldOfPAYBACK/WorldOfPAYBACK/Feature/TransactionList/UI/TransactionListView.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionListView.swift
+//  WorldOfPAYBACK
+//
+//  Created by muhammad Yawar on 1/29/23.
+//
+
+import SwiftUI
+
+struct TransactionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionListView()
+    }
+}
