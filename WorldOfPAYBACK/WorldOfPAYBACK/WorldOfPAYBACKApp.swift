a0200e2d4ff1ee9e707cd7e236d5d87d385e9f75
Yawar M <muhammad.yawar@10pearls.com>
2023-01-29 16:58:32 +0500

    - Initial commit of app creation

---
+++ WorldOfPAYBACK/WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by muhammad Yawar on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
