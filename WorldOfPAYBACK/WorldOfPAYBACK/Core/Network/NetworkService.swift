61d9e93ad931380b6a602c3d04acbf46ac2cdaf9
Yawar M <muhammad.yawar@10pearls.com>
2023-01-29 17:00:37 +0500

    - Add Network service layer

---
+++ WorldOfPAYBACK/WorldOfPAYBACK/Core/Network/NetworkService.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkService.swift
+
+
+import Foundation
+
+protocol NetworkService {
+    
+    func execute<Model: Decodable>(url: URL, completion: @escaping ((Result<Model, Error>) -> Void))
+}
+
+struct NetworkSerivceImpl: NetworkService {
+    
+    let session: URLSession
+    let decoder: JSONDecoder
+    
+    func execute<Model: Decodable>(url: URL, completion: @escaping ((Result<Model, Error>) -> Void)) {
+        
+        session.dataTask(with: url) { data , response, error in
+            
+            if let error = error {
+                
+                completion(.failure(NetworkError.transportError(error)))
+                return
+            }
+            if let response = response as? HTTPURLResponse, !(200..<300).contains(response.statusCode) {
+                
+                completion(.failure(NetworkError.serverError(response.statusCode)))
+                return
+            }
+            guard let data = data else {
+                
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            do {
+                
+                let model = try decoder.decode(Model.self, from: data)
+                completion(.success(model))
+                
+            } catch let error {
+                
+                completion(.failure(NetworkError.decodingError(error)))
+            }
+        }
+        .resume()
+    }
+}
