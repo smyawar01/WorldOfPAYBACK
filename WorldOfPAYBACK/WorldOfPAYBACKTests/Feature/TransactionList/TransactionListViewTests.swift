69dacd93c03a81d53c349052dfa6fdf65cbb9813
Yawar M <muhammad.yawar@10pearls.com>
2023-01-30 04:54:26 +0500

    - Add snapshot testing setup

---
+++ WorldOfPAYBACK/WorldOfPAYBACKTests/Feature/TransactionList/TransactionListViewTests.swift
@@ -0,0 +1,11 @@
+//
+//  TransactionListViewTests.swift
+//  WorldOfPAYBACKTests
+//
+//  Created by muhammad Yawar on 1/30/23.
+//
+
+import XCTest
+
+class TransactionListViewTests: XCTestCase {
+}
